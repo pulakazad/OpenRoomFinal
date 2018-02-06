0350d21b94afd23b79c67c789b0aefae06b1f3f4
pulakazad <pulakazad@gatech.edu>
2018-02-06 13:02:07 +0100

    Initial commit

---
+++ app/src/main/java/fall2017/cs4261/gatech/edu/openroomfinal/MainActivity.java
@@ -0,0 +1,13 @@
+package fall2017.cs4261.gatech.edu.openroomfinal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
