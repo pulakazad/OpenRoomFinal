5083e8e37ee882509f11ded07b1ef6f207a2504a
pulakazad <pulakazad@gatech.edu>
2018-02-06 14:00:36 +0100

    Messages are stored in database and dates are in

---
+++ app/src/main/java/fall2017/cs4261/gatech/edu/openroomfinal/ChatMessage.java
@@ -0,0 +1,51 @@
+package fall2017.cs4261.gatech.edu.openroomfinal;
+
+import java.util.Date;
+
+/**
+ * Created by pulakazad on 2/6/18.
+ */
+
+public class ChatMessage {
+
+    private String messageText;
+    private String messageUser;
+    private long messageTime;
+
+
+    public ChatMessage(String messageText, String messageUser) {
+        this.messageText = messageText;
+        this.messageUser = messageUser;
+
+        // Initialize to current time
+        messageTime = new Date().getTime();
+    }
+
+    public ChatMessage(){
+
+    }
+
+    public String getMessageText() {
+        return messageText;
+    }
+
+    public void setMessageText(String messageText) {
+        this.messageText = messageText;
+    }
+
+    public String getMessageUser() {
+        return messageUser;
+    }
+
+    public void setMessageUser(String messageUser) {
+        this.messageUser = messageUser;
+    }
+
+    public long getMessageTime() {
+        return messageTime;
+    }
+
+    public void setMessageTime(long messageTime) {
+        this.messageTime = messageTime;
+    }
+}
